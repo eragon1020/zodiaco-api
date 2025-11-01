9384eb4ae1d9527098be1cf073af012117c45a9e
eragon1020 <brayan.aragon.zabala@gmail.com>
2025-11-01 03:21:20 -0500

    feat: completar CRUD endpoints y documentación Swagger

--- microservices/characters/src/swagger.js
+++ microservices/characters/src/swagger.js
@@ -1,14 +1,37 @@
-const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Characters Service API',
+      title: 'Saint Seiya Characters API',
       version: '1.0.0',
+      description: 'API para gestionar Caballeros del Zodiaco',
+      contact: {
+        name: 'API Support',
+        email: 'support@saintsapi.com'
+      }
     },
+    servers: [
+      {
+        url: 'https://zodiaco-api.onrender.com',
+        description: 'Production server'
+      },
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server'
+      }
+    ],
+    tags: [
+      {
+        name: 'Characters',
+        description: 'Endpoints para gestionar personajes'
+      }
+    ]
   },
   apis: ['./src/routes/*.js'],
 };
 
-module.exports = swaggerJSDoc(options);
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
